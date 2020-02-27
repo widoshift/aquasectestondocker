ca28b50d1b48009e1b4351637c57e9300a20fba8
widya <widyaunix@gmail.com>
2020-02-27 19:34:17 +0700

    2

--- docker-run-scanner.sh
+++ docker-run-scanner.sh
@@ -1,1 +1,2 @@
-docker run -e "AQUA_SCANNER_LOGICAL_NAME=testaquascan" -d -v /var/run/docker.sock:/var/run/docker.sock registry.birau.com/scanner:4.6 daemon --user scanner --password sc4nnerpwd --host 192.168.122.41
+# host is the docker host ip
+docker run -e "AQUA_SCANNER_LOGICAL_NAME=testaquascan" -d -v /var/run/docker.sock:/var/run/docker.sock registry.birau.com/scanner:4.6 daemon --user scanner --password sc4nnerpwd --host http://192.168.122.41:8080
